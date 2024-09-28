474c8faee0c918de5394a008e75519369656a2ff
KC Ng <nkc900201@gmail.com>
2024-09-28 20:54:26 +0900

    feature: (WIP) set up date input validation

---
+++ src/constants/error-message.contant.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  INVALID_INPUT: "Invalid input",
+  INVALID_DAY: "Day value must be between 1 and 31",
+  INVALID_MONTH: "Month value must be between 1 and 12",
+  INVALID_YEAR: "Year value must not be later than current year"
+}
--- src/hooks/useValidateDateInput.ts
+++ src/hooks/useValidateDateInput.ts
@@ -1,27 +1,53 @@
+import { getYear } from "date-fns"
 import { useState } from "react"
+import { ERROR_MESSAGES } from "../constants/error-message.contant"
 
-
-type DateInputType = {
-  day: string
-  month: string
-  year: string
-}
-
-// TO-DO: implement user validation logic
 function useValidateInput() {
-  const defaultDate: DateInputType = {
-    day: '',
-    month: '',
-    year: ''
+  const [isDateValid, setDateValid] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<string>("")
+
+  // validate date by field (day, month, year)
+  const validateDateField = (value: string, label: string) => {
+
+    if(!value || value.trim().length < 0) {
+      setDateValid(false)
+      setErrorMessage(ERROR_MESSAGES.INVALID_INPUT)
+
+      return
+    }
+
+    const dateValue = parseInt(value.trim(), 10)
+    const currentYear = getYear(new Date())
+
+    if(isNaN(dateValue)) {
+      setDateValid(false)
+      setErrorMessage(ERROR_MESSAGES.INVALID_INPUT)
+
+      return
+    }
+
+    switch(label) {
+      case "DAY":
+        // validate day value is between 1 and 31
+        setDateValid(dateValue >= 1 && dateValue <= 31)
+        break
+      case "MONTH":
+        // validate month value is between 1 and 12
+        setDateValid(dateValue >= 1 && dateValue <= 12)
+        break
+      case "YEAR":
+        // validate year value is not later than current year
+        setDateValid(dateValue <= currentYear)
+        break
+      default:
+        break
+    }
+
+    setDateValid(true)
   }
 
-  const [date, setDate] = useState<DateInputType>(defaultDate)
-  const [isDateValid, setDateValid] = useState<boolean>(false)
 
-  // validate user input date
-
-
-  return {isDateValid, setDateValid, date, setDate}
+  return {isDateValid, errorMessage, validateDateField}
 }
 
 export default useValidateInput
