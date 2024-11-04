7d1496dc8b1a89d268866eb54477c2787ab81aaf
KC Ng <nkc900201@gmail.com>
2024-11-04 17:45:41 +0900

    feature: (WIP) add in to-do list comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css"
 import { Layout, DateInput, CalculateAgeButton, AgeDisplay } from "./components"
 
-// To-do: migrate hooks and validation logic to this component
+/* To-do:
+    1. migrate hooks and validation logic to this component
+    2. Add in props for each child component interface (new)
+    3. Add in error message for each validation failure in UI layout
+    4. Deploy to netlify once 1~3 are completed
+    5. (Optional) add in mobile design layout
+*/
+
 function App() {
   return (
     <Layout>
